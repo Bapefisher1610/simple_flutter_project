aec1af0b8b0167222090dfdb60721352d845dcfd
Bapefisher1610 <sonbape2805@gmail.com>
2024-09-18 22:46:09 +0700

    Điều chỉnh router và thêm endpoint /api/v1/submit vào server

--- backend/bin/server.dart
+++ backend/bin/server.dart
@@ -1,21 +1,72 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-// Configure routes.
-final _router = Router()
+// Cấu hình routes
+final _router = Router(notFoundHandler: _notFoundHandler)
   ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
+  ..get('/api/v1/check', _checkHandler)
+  ..get('/echo/<message>', _echoHandler)
+  ..post('/api/v1/submit', _submitHandler);
+
+//Header mặc định cho dữ liệu trả về dưới dạng JSON
+final _headers = {'Content-Type': 'application/json'};
+
+Response _notFoundHandler(Request req) {
+  return Response.notFound('Khong tim thay duong dan"${req.url}" tren server');
+}
 
 Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
+  return Response.ok(
+    json.encode({'message': 'Hello World!'}),
+    headers: _headers,
+  );
+}
+
+Response _checkHandler(Request req) {
+  return Response.ok(
+    json.encode({'message': 'Chao mung ban den voi ung dung web dong'}),
+    headers: _headers,
+  );
 }
 
 Response _echoHandler(Request request) {
   final message = request.params['message'];
   return Response.ok('$message\n');
+}
+
+Future<Response> _submitHandler(Request req) async {
+  try {
+    final payload = await req.readAsString();
+    final data = json.decode(payload);
+    final name = data['name'] as String?;
+
+    if (name != null && name.isNotEmpty) {
+      final response = {'message': 'Chao mung $name'};
+      return Response.ok(
+        json.encode(response),
+        headers: _headers,
+      );
+    } else {
+      final response = {'message': 'Server khong nhan duoc ten cua ban'};
+      return Response.ok(
+        json.encode(response),
+        headers: _headers,
+      );
+    }
+  } catch (e) {
+    //Xu ly ngoai le khi giai ma JSON
+    final response = {'message': 'Yeu cau khong hop le.Loi ${e.toString()}'};
+
+    //Tra ve phan hoi voi statusCode 400
+    return Response.badRequest(
+      body: json.encode(response),
+      headers: _headers,
+    );
+  }
 }
 
 void main(List<String> args) async {
